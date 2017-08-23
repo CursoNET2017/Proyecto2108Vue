654cead401a96c34a2a3c40a4e63a11af0b95045
unknown <angelfernandezcic@gmail.com>
2017-08-23 16:21:13 +0200

    Crear detalle maestro Banco

---
+++ src/components/DetalleBanco.vue
@@ -0,0 +1,129 @@
+<template>
+    <div id="panel">
+        <div class="linea">
+            <label>Cuenta Bancaria: </label>
+            <input v-if="datapadre.currentCuenta != null" name="cuenta" type="text" v-model="datapadre.currentCuenta.Cuenta" @input="validarCuenta" placeholder="Introduce Cuenta Bancaria">
+            <input v-else name="cuenta" type="text" v-model="datapadre.newCuenta.Cuenta" @input="validarCuenta" placeholder="Introduce Cuenta Bancaria">
+        </div>
+        <div class="linea">
+            <label>Banco: </label>
+            <input v-if="datapadre.currentCuenta != null" name="banco" type="text" v-model="datapadre.currentCuenta.Banco" @input="validarCuenta" placeholder="Introduce Banco">
+            <input v-else name="banco" type="text" v-model="datapadre.newCuenta.Banco" @input="validarCuenta" placeholder="Introduce Banco">
+        </div>
+        <div class="linea">
+            <label>Saldo: </label>
+            <input v-if="datapadre.currentCuenta != null" name="saldo" type="number" v-model="datapadre.currentCuenta.Saldo" @input="validarCuenta" placeholder="Introduce Saldo">
+            <input v-else name="saldo" type="number" v-model="datapadre.newCuenta.Saldo" @input="validarCuenta" placeholder="Introduce Saldo">
+        </div>
+        <div class="linea">
+            <label>Activa: </label>
+            <input name="activa" type="checkbox" v-model="datapadre.newCuenta.Activa" @input="validarCuenta">
+        </div>
+        <div id="botones">
+            <button @click="addCuenta" :disabled="datapadre.currentCuenta || !isValid">Añadir</button>
+            <button @click="updateCuenta" :disabled="!datapadre.currentCuenta || !datapadre.editMode || !isValid">Actualizar</button>
+            <button @click="deleteCuenta" :disabled="!datapadre.currentCuenta">Borrar</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'detalle',
+    props: ['datapadre'],
+    data() {
+        return { isValid: false };
+    },
+    methods: {
+        validarCuenta: function () {
+            var cuentaToValidate = this.datapadre.currentCuenta ? this.datapadre.currentCuenta : this.datapadre.newCuenta;
+            var _isValid = true;
+            if (!(cuentaToValidate.Cuenta.length > 0 && cuentaToValidate.Cuenta.length <= 20)) _isValid = false;
+            if (!(cuentaToValidate.Banco.length > 0 && cuentaToValidate.Banco.length <= 20)) _isValid = false;
+
+            var regExpDNI = /\d{8}[A-Za-z]$/;
+            if (!regExpDNI.test(cuentaToValidate.DNI)) _isValid = false;
+            this.isValid = _isValid;
+        },
+        addCuenta: function () {
+            let _this = this.datapadre;
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:50406/api/CuentaBancarias/',
+                data: _this.newCuenta,
+                success: function (response) {
+                    _this.cuenta.push(response);
+                    _this.newCuenta = { Cuenta: "", Banco: "", Saldo: "", Activa: "" };
+                    _this.backup = JSON.parse(JSON.stringify(_this.cuenta));
+                }
+            });
+        },
+        updateCuenta: function () {
+            let _this = this.datapadre;
+            $.ajax({
+                type: 'PUT',
+                url: 'http://localhost:50406/api/CuentaBancarias/' + _this.currentCuenta.Id,
+                data: _this.currentCuenta,
+                success: function (response) {
+                    var index = _this.backup.findIndex((el) => el.Id == _this.currentCuenta.Id);
+                    _this.cuenta[index] = _this.currentCuenta;
+                    _this.backup = JSON.parse(JSON.stringify(_this.cuenta));
+                    _this.currentCuenta = null;
+                }
+            });
+
+        },
+        deleteCuenta: function () {
+            let _this = this.datapadre;
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://localhost:50406/api/CuentaBancarias/' + _this.currentCuenta.Id,
+                success: function (response) {
+                    var index = _this.backup.findIndex((el) => { el.Id == _this.currentCuenta.Id });
+                    _this.cuenta.splice(index, 1);
+                    _this.backup = JSON.parse(JSON.stringify(_this.cuenta));
+                    _this.currentCuenta = null;
+                }
+            });
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert("Error!->" + errorThrown + "-->" + xhr.responseText);
+        }
+    },
+    created() {
+        this.validarCuenta();
+    }
+}
+
+</script>
+
+<style scoped>
+#panel {
+    margin-left: 5%;
+    width: 45%;
+}
+
+.linea {
+    width: 100%
+}
+
+label,
+input {
+    display: block;
+}
+
+label {
+    width: 25%;
+}
+
+input {
+    margin-left: 5%;
+    width: 55%;
+}
+
+#botones {
+    margin-top: 15px;
+}
+</style>
---
+++ src/components/MaestroBanco.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="maestro">
+
+        <h1>Cuenta Bancaria</h1>
+        <button @click="nuevaCuenta">+Nueva Cuenta</button>
+        <ul>
+            <li v-for="cuenta in datapadre.backup" @click="selectCuenta(cuenta)">
+                {{cuenta.Banco}},{{cuenta.Cuenta}} - {{cuenta.Saldo}} €
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'maestro',
+    props:
+    ['datapadre'],
+    data() {
+        return {};
+    },
+    methods: {
+        getCuentas: function () {
+            let _this = this;
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:50406/api/CuentaBancarias/',
+                success: function (response) {
+                    _this.datapadre.backup =JSON.parse(JSON.stringify(response));
+                    _this.datapadre.cuenta =JSON.parse(JSON.stringify(response));
+                }
+            });
+
+        },
+        selectCuenta: function (cuenta) {
+            this.datapadre.editMode = true;
+            this.datapadre.currentCuenta = JSON.parse(JSON.stringify(cuenta));
+            this.datapadre.newCuenta = { Cuenta: "", Banco: "", Saldo: "", Activa: "" };
+        },
+        nuevaCuenta: function (cuenta) {
+            this.datapadre.editMode = false;
+            this.datapadre.currentCuenta = null;
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert("Error!->" + errorThrown + "-->" + xhr.responseText);
+        }
+    },
+    created() {
+        this.getCuenta();
+    }
+}
+
+</script>
+
+<style scoped>
+#maestro {
+    width: 50%;
+}
+
+h1{
+    width:100%;
+}
+
+ul {
+    width: 100%;
+}
+
+li {
+    list-style-type: none;
+    width:100%;
+}
+
+li:hover {
+    cursor: pointer;
+}
+
+.selected{
+    background-color:brown;
+}
+</style>
